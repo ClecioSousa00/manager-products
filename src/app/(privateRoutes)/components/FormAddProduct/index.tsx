48d0ad85e300faff95fa195363ac0304291ef99b
ClecioSousa00 <cleciosouza015@gmail.com>
2025-01-02 20:37:50 -0300

    Feat: add layout add product

---
+++ src/app/(privateRoutes)/components/FormAddProduct/index.tsx
@@ -0,0 +1,119 @@
+import * as SheetUi from '@/components/ui/sheet'
+import { Button } from '@/components/ui/button'
+
+import { IconPlus } from '@/iconsSvg/IconPlus'
+import { Label } from '@/components/ui/label'
+import * as SelectUi from '@/components/ui/select'
+import { optionsCategory } from '../TableProducts/optionsCategory'
+import { Input } from '@/components/ui/input'
+
+export const FormAddProduct = () => {
+  const handleSelectCategory = (category: string) => {
+    console.log(category)
+  }
+  return (
+    <SheetUi.Sheet>
+      <SheetUi.SheetTrigger asChild>
+        <Button>
+          <IconPlus />
+          <span className="hidden md:block" title="Adicionar Produto">
+            Adicionar Produto
+          </span>
+        </Button>
+      </SheetUi.SheetTrigger>
+      <SheetUi.SheetContent className="w-[90%] md:max-w-[420px] h-full px-0">
+        <SheetUi.SheetHeader>
+          <SheetUi.SheetTitle className="text-left pl-5">
+            Adicionar Novo Produto
+          </SheetUi.SheetTitle>
+        </SheetUi.SheetHeader>
+        <div className="h-full flex flex-col justify-between pt-4">
+          <form action="" className=" px-5 flex flex-col  gap-4 ">
+            <div className="flex flex-col gap-2 ">
+              <Label
+                htmlFor="category"
+                className="text-xs text-left text-gray-light"
+              >
+                Categoria
+                <span className="text-danger"> *</span>
+              </Label>
+              <SelectUi.Select onValueChange={handleSelectCategory}>
+                <SelectUi.SelectTrigger className="w-full  text-gray-light">
+                  <SelectUi.SelectValue placeholder="Selecione uma categoria"></SelectUi.SelectValue>
+                </SelectUi.SelectTrigger>
+                <SelectUi.SelectContent>
+                  {optionsCategory.map((category) => (
+                    <SelectUi.SelectItem key={category.id} value={category.id}>
+                      {category.label}
+                    </SelectUi.SelectItem>
+                  ))}
+                </SelectUi.SelectContent>
+              </SelectUi.Select>
+            </div>
+            <div className="flex flex-col gap-2">
+              <Label
+                htmlFor="nameProduct"
+                className="text-xs text-left text-gray-light"
+              >
+                Nome
+                <span className="text-danger"> *</span>
+              </Label>
+              <Input
+                id="nameProduct"
+                placeholder="Informe o nome do produto"
+                className="text-sm text-gray-light placeholder:text-gray-light font-normal"
+              />
+            </div>
+            <div className="flex flex-col gap-2">
+              <Label
+                htmlFor="picture"
+                className="text-xs text-left text-gray-light"
+              >
+                Imagem
+              </Label>
+              <Input
+                id="picture"
+                type="file"
+                className="file:text-gray-light pl-1 py-0 pt-[5px] -mt-[3px]  px-0 text-gray-light text-xs font-normal file:text-sm  file:font-normal"
+              />
+            </div>
+            <div className="flex flex-col gap-2">
+              <Label
+                htmlFor="amount"
+                className="text-xs text-left text-gray-light"
+              >
+                Quantidade
+                <span className="text-danger"> *</span>
+              </Label>
+              <Input
+                id="amount"
+                placeholder="Informe a quantidade"
+                className="text-sm text-gray-light placeholder:text-gray-light font-normal"
+              />
+            </div>
+            <div className="flex flex-col gap-2">
+              <Label
+                htmlFor="price"
+                className="text-xs text-left text-gray-light"
+              >
+                Preço
+                <span className="text-danger"> *</span>
+              </Label>
+              <Input
+                id="price"
+                placeholder="Informe o preço do produto"
+                className="text-sm text-gray-light placeholder:text-gray-light font-normal"
+              />
+            </div>
+          </form>
+          <div className="flex  items-center justify-center shadow-2xl shadow-black/55 gap-2 py-6">
+            <SheetUi.SheetClose asChild>
+              <Button variant={'outline'}>Cancelar</Button>
+            </SheetUi.SheetClose>
+            <Button>Adicionar</Button>
+          </div>
+        </div>
+      </SheetUi.SheetContent>
+    </SheetUi.Sheet>
+  )
+}
--- src/app/(privateRoutes)/components/TableProducts/index.tsx
+++ src/app/(privateRoutes)/components/TableProducts/index.tsx
@@ -4,15 +4,13 @@ import { Search } from 'lucide-react'
 
 import { colors } from '@/styles/colors'
 
-import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Separator } from '@/components/ui/separator'
-
-import { IconPlus } from '@/iconsSvg/IconPlus'
 
 import { ContainerTable } from '../Table/ContainerTable'
 import { TableContent } from '../Table/TableContent'
 import { FormFilter } from '../FormFilter'
+import { FormAddProduct } from '../FormAddProduct'
 
 export const TableProducts = () => {
   return (
@@ -25,12 +23,7 @@ export const TableProducts = () => {
           handleClickIcon={() => console.log('search')}
         />
         <div className="space-x-2 flex items-center">
-          <Button>
-            <IconPlus />
-            <span className="hidden md:block" title="Adicionar Produto">
-              Adicionar Produto
-            </span>
-          </Button>
+          <FormAddProduct />
           <FormFilter />
         </div>
       </div>
